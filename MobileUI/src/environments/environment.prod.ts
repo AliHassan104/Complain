a88c2f2bb9a21ab4ae3ddf5d7d9f78abb8c57649
shazadiqbal <shahzadiqbal.may18@gmail.com>
2022-10-06 21:01:18 +0500

    prod env changes

--- MobileUI/src/environments/environment.prod.ts
+++ MobileUI/src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
+
 export const environment = {
-  // production: true,
-  production:false,
-  baseUrl: 'https://' + window.location.hostname+'/mobileapp/' ,
+  //production: false,
+  baseUrl: 'http://' + window.location.hostname + ':8081',
+ // baseUrl: 'http://localhost:8081',
+    // baseUrl: 'https://d633-101-53-235-105.ap.ngrok.io',
   firebase: {
     apiKey: "AIzaSyB8t7wlWQpISipd9RyOwLIdcw4MNQ5B-aA",
     authDomain: "complain-management-bfaa7.firebaseapp.com",
