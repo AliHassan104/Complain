07aceb2f2ec601b9e1bf2f567f7adc540d6abf77
shazadiqbal <shahzadiqbal.may18@gmail.com>
2022-10-06 21:15:20 +0500

    shazad changes

--- MobileUI/src/environments/environment.prod.ts
+++ MobileUI/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 
 export const environment = {
   production: true,
-  test:true,
   baseUrl: 'https://' + window.location.hostname + ':8081',
  // baseUrl: 'http://localhost:8081',
     // baseUrl: 'https://d633-101-53-235-105.ap.ngrok.io',
