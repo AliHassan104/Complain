f326376372f5dd5d31687e2f5eb7d18da73a8193
AliHassan104 <alihassan.uok@gmail.com>
2022-11-02 22:56:43 +0500

    revert sheri changes

--- UI/cpanel/js/properties.js
+++ UI/cpanel/js/properties.js
@@ -1,7 +1,7 @@
-//  baseUrl = `https://api.jicomplain.com`
-//  loginUrl = `https://admin.jicomplain.com/`
-baseUrl=`http://localhost:8082`
-loginUrl=`http://127.0.0.1:5500`
+  baseUrl = `https://api.jicomplain.com`
+  loginUrl = `https://admin.jicomplain.com/`
+//baseUrl=`http://localhost:8081`
+//loginUrl=`http://127.0.0.1:5500`
 
 exception404 = `${loginUrl}/pages-404.html`
 loginPage =   `${loginUrl}/loginpage.html`
